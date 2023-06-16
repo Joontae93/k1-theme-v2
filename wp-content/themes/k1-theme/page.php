faa6d4fe5d3131c42d52c01413c9deab9780d40c
Joontae93 <kj.roelke@gmail.com>
2023-06-15 19:36:29 -0500

    update placeholder content

--- wp-content/themes/k1-theme/page.php
+++ wp-content/themes/k1-theme/page.php
@@ -8,11 +8,18 @@
 
 $content = new Content_Sections();
 get_header(); ?>
-<main class="site-content <?php echo $post->post_title; ?>">
-	<?php // $content->hero_section( get_the_ID() ); ?>
+<main class="site-content <?php echo $post->post_name; ?>">
+	<section class="hero d-flex align-items-stretch text-white bg-primary--dark" style="height:60rem;">
+		<div class="container d-flex flex-column justify-content-center">
+			<div class="row flex-column justify-content-center">
+				<h1 class="headline">Page Title: <?php echo get_the_title(); ?></h1>
+				<span class='subheadline'>A dummy hero section for now</span>
+			</div>
+		</div>
+	</section>
 	<?php
-	switch ( $post->post_title ) {
-		case ( 'About' ):
+	switch ( $post->post_name ) {
+		case ( 'about' ):
 			get_template_part( 'templates/page', 'about' );
 			break;
 		default:
