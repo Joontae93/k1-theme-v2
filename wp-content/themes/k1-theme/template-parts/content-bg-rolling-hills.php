e88d034d60f39774e61cfc92e32e0b88d72ec69e
Joontae93 <kj.roelke@gmail.com>
2023-06-29 20:15:41 -0500

    fixes hollow out bg-rolling-hills #8

--- wp-content/themes/k1-theme/template-parts/content-bg-rolling-hills.php
+++ wp-content/themes/k1-theme/template-parts/content-bg-rolling-hills.php
@@ -5,41 +5,35 @@
 
 $background_image = k1_get_image_asset_url( 'bg-rolling-hills', 'svg', 'bg-images', false );
 extract( $args );
+
+$headline    = esc_textarea( $headline );
+$subheadline = esc_textarea( $subheadline );
+$cta['text'] = esc_textarea( $cta['text'] );
+$cta['link'] = esc_url( $cta['link'] );
+$row_2       = acf_esc_html( $row_2 );
+
+$classes = 'bg-rolling-hills';
+if ( isset( $class ) && ! empty( $class ) ) {
+	$classes .= ' ' . $class;
+}
+
 ?>
-<section class="the-plan bg-rolling-hills" id="healthy-growth" style="background-image:url('<?php echo $background_image; ?>')">
+<section class="<?php echo $classes; ?> " id="<?php echo $id ?? ''; ?>" style="background-image:url('<?php echo $background_image; ?>')">
 	<div class="container">
 		<div class="row">
 			<div class="col align-items-stretch">
-				<div class="the-plan__content">
+				<div class="<?php echo $class . '__content'; ?>">
 					<h2 class="text-white"><?php echo $headline; ?></h2>
 					<span class="subheadline text-white"><?php echo $subheadline; ?></span>
-					<a class="pill-btn__fill--primary" href="#">Get Started</a>
+					<a class="pill-btn__fill--primary" href="<?php echo $cta['link']; ?>">
+						<?php echo $cta['text']; ?>
+					</a>
 				</div>
-				<div class="scoll-reveal text-white">
-					<?php
-					$scroll_items = array(
-						array(
-							'image' => 'healthygrowth',
-							'text'  => 'Healthy Growth',
-						),
-						array(
-							'image' => 'staffculture',
-							'text'  => 'Courageous Staff Culture',
-						),
-						array(
-							'image' => 'education',
-							'text'  => 'Education that sparks vision',
-						),
-					);
-					foreach ( $scroll_items as $index => $item ) {
-						$url = k1_get_image_asset_url( $item['image'], 'svg', '', false );
-						echo "<div class='scroll-reveal__item' data-aos='fade-in'>
-						<img src='{$url}' class='scroll-reveal__icon' data-aos='fade-right'>
-						<h3 class='headline' data-aos='fade-right'>{$item['text']}</h3>
-					</div>";
-					}
-					?>
-				</div>
+			</div>
+		</div>
+		<div class="row <?php echo empty( $class ) ? 'bg-rolling-hills__row-2' : $class . '__row-2'; ?>">
+			<div class="col text-white">
+				<?php echo $row_2; ?>
 			</div>
 		</div>
 	</div>
