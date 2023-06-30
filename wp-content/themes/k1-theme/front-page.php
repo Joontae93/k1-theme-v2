2ba370fb35662061aafda802d83762e697f348df
Joontae93 <kj.roelke@gmail.com>
2023-06-29 19:53:28 -0500

    hollow front-page

--- wp-content/themes/k1-theme/front-page.php
+++ wp-content/themes/k1-theme/front-page.php
@@ -11,45 +11,7 @@ k1_enqueue_page_assets( 'frontPage' );
 
 get_header(); ?>
 <main class="site-content">
-	<section class="hero">
-		<div class="hero__background">
-			<div class="hero__background--lower" style="background-image: url('<?php k1_get_image_asset_url( 'homepage--hero', 'jpg', 'bg-images' ); ?>');"></div>
-			<div class="hero__background--upper"></div>
-		</div>
-		<div class="hero__content container-fluid d-flex align-items-stretch">
-			<div class="row">
-				<div class="col-2 d-flex flex-column">
-					<img src="<?php k1_get_image_asset_url( 'heroimgleft', 'svg' ); ?>" class="mt-auto" />
-				</div>
-				<div class="col d-flex flex-column justify-content-center">
-					<div class="container">
-						<div class="hero__text">
-							<h1 class="hero__text--headline text-white">kingdom one</h1>
-							<span class="hero__text--subheadline-1 subheadline white-stroke">Growing the church together</span>
-							<p class="hero__text--subheadline-2 subheadline text-primary">Regain peace of mind in your HR and Finance ministries with coaching, education, and strategy
-								built for you!
-							</p>
-							<a class="pill-btn__fill--primary" href="#">Get Started</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-4 d-flex flex-column">
-					<img src="<?php k1_get_image_asset_url( 'heroimgright', 'svg' ); ?>">
-				</div>
-			</div>
-		</div>
-		<aside class="quick-sell py-5">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-lg-3 d-flex flex-column justify-content-center align-items-center">IMAGE!</div>
-					<div class="col-lg-6 d-flex flex-column justify-content-center align-items-start">
-						<h2 class="headline text-white">Training Your Staff Should be quick & efficient</h2>
-						<a href="#" class="pill-btn__outline--white">Download</a>
-					</div>
-				</div>
-			</div>
-		</aside>
-	</section>
+	<?php get_template_part( 'template-parts/front-page/section', 'hero' ); ?>
 
 	<section class="selling-points">
 		<div class="container">
@@ -83,7 +45,16 @@ get_header(); ?>
 			</div>
 		</div>
 	</section>
-	<?php get_template_part( 'template-parts/content', 'bg-rolling-hills', array() ); ?>
+	<?php
+	get_template_part(
+		'template-parts/content',
+		'bg-rolling-hills',
+		array(
+			'headline'    => 'Courageous, Healthy & Effective',
+			'subheadline' => 'Our Tools, Training & Talent deliver the expertise you need to develop a healthy ministry. ',
+		)
+	);
+	?>
 	<?php get_template_part( 'template-parts/swiper', 'testimonials' ); ?>
 	<?php get_template_part( 'template-parts/swiper', 'brands' ); ?>
 	<section class="problems">
