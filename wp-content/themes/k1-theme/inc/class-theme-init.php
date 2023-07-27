15f2e9239925d6d017744c4fb385e7d20d4d8084
Joontae93 <kj.roelke@gmail.com>
2023-07-27 17:01:33 -0500

    clean up classes

--- wp-content/themes/k1-theme/inc/class-theme-init.php
+++ wp-content/themes/k1-theme/inc/class-theme-init.php
@@ -1,12 +1,11 @@
 <?php // phpcs:ignore
-class Theme_Init {
-	// phpcs:ignore
+require_once dirname(__FILE__) . '/class-k1-theme-cleaner.php';
+class Theme_Init extends K1_Theme_Cleaner {
 	function __construct() {
 		$this->load_files();
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_k1_scripts' ) );
+		add_action( 'after_setup_theme', array( $this, 'k1_theme_supports' ) );
 		add_action( 'after_setup_theme', array( $this, 'register_k1_menus' ) );
-		add_theme_support( 'post-thumbnails' );
-		$this->disable_discussion();
 		add_action( 'init', array( $this, 'alter_post_types' ) );
 	}
 
@@ -16,11 +15,11 @@ class Theme_Init {
 		require_once get_template_directory() . '/inc/class-k1-nav-walker.php';
 		require_once get_template_directory() . '/inc/theme-functions.php';
 	}
+
 	/**
 	 * Adds scripts with the appropriate dependencies
 	 */
 	public function enqueue_k1_scripts() {
-		// Get modification time. Enqueue files with modification date to prevent browser from loading cached scripts and styles when file content changes.
 		$modified_styles  = gmdate( 'YmdHi', filemtime( get_stylesheet_directory() . '/dist/global.css' ) );
 		$modified_scripts = gmdate( 'YmdHi', filemtime( get_stylesheet_directory() . '/dist/global.js' ) );
 
@@ -37,15 +36,10 @@ class Theme_Init {
 		$this->remove_wordpress_styles( array( 'classic-theme-styles', 'wp-block-library', 'dashicons', 'global-styles' ) );
 	}
 
-	/**
-	 * Provide an array of handles to dequeue
-	 *
-	 * @param array $handles the handles of scripts to dequeue
-	 */
-	private function remove_wordpress_styles( array $handles ) {
-		foreach ( $handles as $handle ) {
-			wp_dequeue_style( $handle );
-		}
+	/** Handle Theme Supports */
+	public function k1_theme_supports() {
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'title-tag' );
 	}
 
 	public function register_k1_menus() {
@@ -56,45 +50,5 @@ class Theme_Init {
 				'footer_menu'  => __( 'Footer Menu', 'k1' ),
 			)
 		);
-	}
-	public function alter_post_types() {
-		$post_types = array( 'post', 'page' );
-		foreach ( $post_types as $post_type ) {
-			$this->disable_post_type_support( $post_type );
-		}
-	}
-	public function disable_discussion() {
-		// Close comments on the front-end
-		add_filter( 'comments_open', '__return_false', 20, 2 );
-		add_filter( 'pings_open', '__return_false', 20, 2 );
-
-		// Hide existing comments
-		add_filter( 'comments_array', '__return_empty_array', 10, 2 );
-
-		// Remove comments page in menu
-		add_action(
-			'admin_menu',
-			function () {
-				remove_menu_page( 'edit-comments.php' );
-			}
-		);
-
-		// Remove comments links from admin bar
-		add_action(
-			'init',
-			function () {
-				if ( is_admin_bar_showing() ) {
-					remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
-				}
-			}
-		);
-	}
-	private function disable_post_type_support( $post_type ) {
-		$supports = array( 'comments', 'trackbacks', 'revisions' );
-		foreach ( $supports as $support ) {
-			if ( post_type_supports( $post_type, $support ) ) {
-				remove_post_type_support( $post_type, $support );
-			}
-		}
 	}
 }
