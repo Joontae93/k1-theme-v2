b87343039b8be3e5e4b63361544b74a7a8708d96
Alexis Reed <alexisritareed@gmail.com>
2023-07-29 12:10:31 -0700

    Edits to Home Page Fixes #14

--- wp-content/themes/k1-theme/front-page.php
+++ wp-content/themes/k1-theme/front-page.php
@@ -18,7 +18,7 @@ get_header(); ?>
 			<div class="row">
 				<div class="selling-points__figures col-4">
 					<img src="<?php k1_get_image_asset_url( 'brokenleaves', 'png' ); ?>">
-					<div class="script-values">
+					<!--<div class="script-values">
 						<?php
 						$values = array( 'Calling', 'Connection', 'Courage', 'Culture' );
 						foreach ( $values as $index => $value ) {
@@ -27,10 +27,10 @@ get_header(); ?>
 							echo "<img src='{$url}' class='script-values__value' data-aos='fade-down' data-aos-delay='{$delay}' />";
 						}
 						?>
-					</div>
+					</div>-->
 				</div>
 				<div class="col">
-					<h2 class="headline">Ministry is <span class="text-primary d-block">Complex</span></h2>
+					<h2 class="headline">Our Ministry First Approach <br> Helps You <span class="text-primary d-block">Avoid</span>:</h2>
 					<div class="row selling-points__underlines">
 						<div class="col">
 							<hr class="selling-points__underlines--line-1">
@@ -40,8 +40,21 @@ get_header(); ?>
 						</div>
 						<div class="col-4"></div>
 					</div>
-					<p>Are you ready to simplify? Our approach is simple; we use marketplace tools, resources, and experience with a heart for healthy ministry.</p>
+					<p>
+						<ul>
+							<li>High turnover rate</li>
+							<li>Unfruitful staffing search</li>
+							<li>Painful leadership transitions, terminations, & underperformance</li>
+							<li>Unstable work environments & unhealthy staff culture</li>
+							<li>Poor financial visibility</li>
+							<li>Ineffective marketing & communications</li>
+							<li>Frustrating piecemeal systems and process</li>
+						</ul>
+					</p>
 				</div>
+			</div>
+			<div class="row">
+				<p>Don't let ministry pain points stop you from fulfilling your calling. At Kingdom One, we help ministries reduce turnover, find quality candidates and assist with hiring & transitions. Transform your ministry work environment to be more courageous, healthy & effective with our HR, finance, and strategic experts. </p>
 			</div>
 		</div>
 	</section>
@@ -58,7 +71,7 @@ get_header(); ?>
 				'text' => 'Get Started',
 				'link' => '/get-started',
 			),
-			'row_2'       => 'big text area',
+			'row_2'       => 'Expert Guidance with a Kingdom One Ministry Partner Expert Guidance with a Kingdom One Ministry Partner Expert Guidance with a Kingdom One Ministry Partner Expert Guidance with a Kingdom One Ministry Partner Expert Guidance with a Kingdom One Ministry Partner ',
 		)
 	);
 	?>
@@ -86,51 +99,19 @@ get_header(); ?>
 		)
 	);
 	?>
-	<?php get_template_part( 'template-parts/swiper', 'testimonials' ); ?>
-	<?php get_template_part( 'template-parts/swiper', 'brands' ); ?>
-	<section class="problems">
-		<div class="container text-center">
-			<div class="row justify-content-center mb-5">
-				<div class="col-10">
-					<h2>Has ministry gotten <span class="text-primary">complicated</span> for you and your team?
-					</h2>
-					<p class="subheadline my-5">Ministry life is a complicated life! Can we get an "amen"? While industry best practices and guidance is an excellent start, the pursuit
-						of
-						becoming courageous,
-						healthy, and effective is nuanced. Our team is ready to partner with you in a 1:1 capacity to assess and coach you forward thoroughly.</p>
+	<section class="">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 text-center">
+					<h2 class="text-primary">We're using our marketplace and <br> ministry expertise to help <br> ministries become more <span class="text-primary d-block">Courageous, Healthy & Effective</span></h2>
 				</div>
 			</div>
-			<div class="row my-5">
-				<?php
-				$steps = array(
-					array(
-						'image'       => k1_get_image_asset_url( 'resources', 'svg', echo: false ),
-						'headline'    => 'Grab some free resources',
-						'subheadline' => 'Get to know us with free tools education, and resources',
-					),
-					array(
-						'image'       => k1_get_image_asset_url( 'tools', 'svg', echo: false ),
-						'headline'    => 'Grab tools &amp; Courses',
-						'subheadline' => 'Grab a tool or course to sharpen your expertise',
-					),
-					array(
-						'image'       => k1_get_image_asset_url( 'talents', 'svg', echo: false ),
-						'headline'    => 'Grab some talents',
-						'subheadline' => 'Grab some talents to help get the work done',
-					),
-				);
-				?>
-				<?php foreach ( $steps as $step ) : ?>
-				<div class="col-lg-4">
-					<img src="<?php echo $step['image']; ?>">
-					<h3><?php echo $step['headline']; ?></h3>
-					<p><?php echo $step['subheadline']; ?></p>
-				</div>
-				<?php endforeach; ?>
-			</div>
-			<a class="pill-btn__fill--primary mt-auto" href="#">Get Started</a>
 		</div>
 	</section>
+
+	<?php get_template_part( 'template-parts/swiper', 'testimonials' ); ?>
+	<?php get_template_part( 'template-parts/swiper', 'brands' ); ?>
+	<?php get_template_part( 'template-parts/content', 'three-steps'); ?>
 	<?php get_template_part( 'template-parts/content', 'final-cta' ); ?>
 </main>
 
