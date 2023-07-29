27ff760d64411bff2576abbb01fc11941f2dcbf8
Alexis Reed <alexisritareed@gmail.com>
2023-07-29 10:20:13 -0700

    Images for backgrounds not appearing

--- wp-content/themes/k1-theme/templates/page-communications.php
+++ wp-content/themes/k1-theme/templates/page-communications.php
@@ -101,7 +101,7 @@
             )
         );
 	    ?>
-        <section>
+        <section class="statistics">
             <div class="container">
                 <div class="row text-center">
                     <div class="col-2"></div>
@@ -126,50 +126,8 @@
                     <div class="col-2"></div>
                 </div>
             </div>
-        </section>
-		<section class="threesteps">
-			<div class="threesteps__background">
-				<div class="threesteps__background--lower"></div>
-				<div class="threesteps__background--upper"></div>
-			</div>
-			<div class="container threesteps__content">
-				<div class="row text-center">
-					<div class="col d-flex justify-content-center align-items-center">
-						<h2 class="h1 headline text-white">Are You Ready To Make <br> A Positive Marketing Change?</h2>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-2">
-						<img src="<?php k1_get_image_asset_url( 'assessment', 'svg' ); ?>">
-					</div>
-					<div class="col-2"></div>
-					<div class="col-8">
-						<h3 class="text-primary">Take a Marketing and Communications <br> assessment to evaluate your Marcom efforts.</h3>
-						<h4 class="text-white">This free quick assessment is the jump start to help identify where to update your marketing and comms efforts.</h4>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-2">
-						<img src="<?php k1_get_image_asset_url( 'plan', 'svg' ); ?>">
-					</div>
-					<div class="col-2"></div>
-					<div class="col-8">
-						<h3 class="text-primary">Create a plan with Kingdom One Marcom</h3>
-						<h4 class="text-white">Visit the Kingdom One get started page and set up a time to meet with a ministry marketing partner and plan for your design, website, social media, and marketing for your ministry! Let's go!</h4>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-2">
-						<img src="<?php k1_get_image_asset_url( 'marketingframework', 'svg' ); ?>">
-					</div>
-					<div class="col-2"></div>
-					<div class="col-8">
-						<h3 class="text-primary">Enjoy your new marketing framework that <br> makes ministry marketing 100x easier.</h3>
-						<h4 class="text-white">Moving from a broken marketing system to an effective lead machine is joyful and gives you more capacity to work on the ministry you love.</h4>
-					</div>
-				</div>
-			</div>
-		</section>
+        </section> 
+       <?php get_template_part("template-parts/content", "three-steps"); ?>
        <?php get_template_part("template-parts/swiper", "brands"); ?>
        <?php get_template_part("template-parts/content", "final-cta") ?>
 </main>
