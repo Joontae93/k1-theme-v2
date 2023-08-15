ccd145a4ab476782fcf3a27cbc0420066cbc8dc1
kjroelke <kj.roelke@gmail.com>
2023-08-15 10:20:03 -0500

    finish single.php

--- wp-content/themes/k1-theme/single.php
+++ wp-content/themes/k1-theme/single.php
@@ -3,8 +3,29 @@
  * Post Single Template
  */
 
+$content = new Content_Sections();
 get_header();
-the_post_thumbnail();
-the_title();
-// the_content();
+?>
+<?php
+$content->hero_section(
+	null,
+	true,
+	array(
+		'has_background_image' => false,
+		'subheadline'          => '',
+		'color_direction'      => 'left',
+		'color'                => 'primary',
+		'has_cta'              => false,
+	)
+);
+?>
+<article class="container my-5 p-5">
+	<div class="row">
+		<?php
+		the_post_thumbnail();
+		the_content();
+		?>
+	</div>
+</article>
+<?php
 get_footer();
