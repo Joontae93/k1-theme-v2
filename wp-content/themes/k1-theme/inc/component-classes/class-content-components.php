0d412ec38d4003066014ffc3a80adcd8b08bc3da
kjroelke <kj.roelke@gmail.com>
2023-08-15 10:17:44 -0500

    finish blog archive

--- wp-content/themes/k1-theme/inc/component-classes/class-content-components.php
+++ wp-content/themes/k1-theme/inc/component-classes/class-content-components.php
@@ -122,7 +122,7 @@ class Content_Components {
 	}
 
 	/** Gets the content layer of the Hero Section */
-	public function get_hero_content( string $headine, string $subheadline, bool $has_cta, array $cta_options = array() ) : string {
+	public function get_hero_content( string $headine, string $subheadline, bool $has_cta, array|null $cta_options = array() ) : string {
 		$markup  = "<div class='hero__content container d-flex flex-column align-items-stretch'><div class='row'><div class='col-1 align-self-start h-auto position-relative d-none d-md-block'>" . k1_get_svg_asset( 'leaves-3', false, false ) . "</div><div class='position-relative d-flex flex-column col-11'>";
 		$markup .= $this->headline(
 			$headine,
--- wp-content/themes/k1-theme/index.php
+++ wp-content/themes/k1-theme/index.php
@@ -4,15 +4,45 @@
  */
 
 get_header();
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post();
-		the_post_thumbnail();
-		the_title();
-		the_excerpt();
-		echo 'hello';
-	}
-} else {
-	echo '<p>No posts found!';
-}
+$content = new Content_Sections();
+?>
+<?php
+$content->hero_section(
+	null,
+	true,
+	array(
+		'has_background_image' => false,
+		'alternate_headline'   => 'Free Resources',
+		'subheadline'          => 'Jumpstart your ministry',
+		'color_direction'      => 'left',
+		'color'                => 'primary',
+		'has_cta'              => false,
+	)
+);
+?>
+<?php if ( have_posts() ) : ?>
+<div class="container">
+	<ul class="list-unstyled row gap-lg-5 my-5 justify-content-lg-evenly">
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
+		<a href="<?php the_permalink(); ?>" class='p-3 d-block col-lg-3 border-primary border border-3 flex-grow-1'>
+			<li>
+				<?php
+				the_post_thumbnail();
+				the_title( '<h2 class="headline h4 fs-2 mb-4 color-grey">', '</h2>' );
+				?>
+				<p class="fs-5"><?php echo get_the_excerpt(); ?></p>
+			</li>
+		</a>
+
+
+		<?php endwhile; ?>
+	</ul>
+</div>
+<?php else : ?>
+<p>No posts found!</p>
+	<?php
+endif;
 get_footer();
