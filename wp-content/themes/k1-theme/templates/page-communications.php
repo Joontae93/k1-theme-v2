7574f46c31198551e91532fe261a7848967c91dd
kjroelke <kj.roelke@gmail.com>
2024-06-26 19:55:15 -0500

    update marcom page copy

--- wp-content/themes/k1-theme/templates/page-communications.php
+++ wp-content/themes/k1-theme/templates/page-communications.php
@@ -9,14 +9,14 @@ $content = new Content_Sections();
 ?>
 
 <section class="problems">
-	<div class="container">
-		<div class="row my-5">
-			<div class="col">
-				<h2 class="text-center">is your marketing</h2>
-			</div>
-		</div>
-		<div class="row problems__content justify-content-around">
-			<?php
+    <div class="container">
+        <div class="row my-5">
+            <div class="col">
+                <h2 class="text-center">is your marketing</h2>
+            </div>
+        </div>
+        <div class="row problems__content justify-content-around">
+            <?php
 			$issues = array(
 				array(
 					'svg'  => 'ignored',
@@ -43,34 +43,34 @@ $content = new Content_Sections();
 				echo "<div class='col-sm-12 col-lg-2 d-flex flex-column align-items-center'>" . k1_get_svg_asset( 'marcom-' . $issue['svg'], false, false ) . "<p class='text-center'>{$issue['text']}</p></div>";
 			}
 			?>
-		</div>
-		<div class="row my-5 py-5 justify-content-center">
-			<div class="col-lg-11">
-				<p class='text-center'><b>Marketing and communication for ministry takes a lot of work.</b><br />Let’s cut through the noise of social media and busy lives to showcase your
-					ministry strengths and make an eternal impact on people’s souls!</p>
-			</div>
-		</div>
-	</div>
+        </div>
+        <div class="row my-5 py-5 justify-content-center">
+            <div class="col-lg-11">
+                <p class='text-center'><b>Marketing and communication for ministry takes a lot of work.</b><br />Let’s cut through the noise of social media and busy lives to showcase your
+                    ministry strengths and make an eternal impact on people’s souls!</p>
+            </div>
+        </div>
+    </div>
 </section>
 <section class="marcom-frameworks">
-	<?php $content->get_color_background_layers( 'marcom-frameworks', 'zig-zag-right', array( 'marcom-frameworks-bg', 'webp' ) ); ?>
-	<div class="container marcom-frameworks__content position-relative z-2 py-5">
-		<div class="row my-5">
-			<div class="d-none d-md-block col-1"><?php k1_get_svg_asset( 'icon-growth' ); ?></div>
-			<div class="col">
-				<h2 class="headline text-lg-center text-white text-lowercase">Spark New Growth with Kingdom One's Marketing and Communications Frameworks!</h2>
-			</div>
-			<div class="d-none d-md-block col-1">
-				<?php k1_get_svg_asset( 'leaves-3' ); ?>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-1">
-				<!-- Spacer -->
-			</div>
-			<div class="col-lg-8">
-				<h3 class="h4 text-white text-poppins">Get a Proven Framework for marketing & communication:</h3>
-				<?php
+    <?php $content->get_color_background_layers( 'marcom-frameworks', 'zig-zag-right', array( 'marcom-frameworks-bg', 'webp' ) ); ?>
+    <div class="container marcom-frameworks__content position-relative z-2 py-5">
+        <div class="row my-5">
+            <div class="d-none d-md-block col-1"><?php k1_get_svg_asset( 'icon-growth' ); ?></div>
+            <div class="col">
+                <h2 class="headline text-lg-center text-white text-lowercase">Spark New Growth with Kingdom One's Marketing and Communications Frameworks!</h2>
+            </div>
+            <div class="d-none d-md-block col-1">
+                <?php k1_get_svg_asset( 'leaves-3' ); ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-1">
+                <!-- Spacer -->
+            </div>
+            <div class="col-lg-8">
+                <h3 class="h4 text-white text-poppins">Get a Proven Framework for marketing & communication:</h3>
+                <?php
 				$content->bulleted_list(
 					array(
 						'Systems for social media, email, website, and live event marketing',
@@ -84,27 +84,27 @@ $content = new Content_Sections();
 					'mt-3 mb-5'
 				);
 				?>
-			</div>
-		</div>
-		<div class="row justify-content-center"><?php $content->cta_button(); ?></div>
-	</div>
+            </div>
+        </div>
+        <div class="row justify-content-center"><?php $content->cta_button(); ?></div>
+    </div>
 </section>
 <section class="empathy">
-	<div class="container">
-		<div class="row justify-content-lg-center">
-			<div class="col-lg-11 text-center">
-				<h2 class="text-poppins">We Know What It Feels Like</h2>
-				<div class="subheadline color-grey fw-bold text-poppins my-5">
-					Working hard for an event, product, or service and have marketing fail to promote it to the right people takes a toll on you.
-				</div>
-				<p class="mb-5 col-lg-10 mx-auto">We can teach and implement a story-based framework like the ones we've created for dozens of ministries that helped them gain confidence in
-					their
-					messaging
-					and results with their launches.</p>
-				<?php $content->cta_button(); ?>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row justify-content-lg-center">
+            <div class="col-lg-11 text-center">
+                <h2 class="text-poppins">We Know What It Feels Like</h2>
+                <div class="subheadline color-grey fw-bold text-poppins my-5">
+                    Working hard for an event, product, or service and have marketing fail to promote it to the right people takes a toll on you.
+                </div>
+                <p class="mb-5 col-lg-10 mx-auto">We can teach and implement a story-based framework like the ones we've created for dozens of ministries that helped them gain confidence in
+                    their
+                    messaging
+                    and results with their launches.</p>
+                <?php $content->cta_button(); ?>
+            </div>
+        </div>
+    </div>
 </section>
 <?php
 get_template_part(
@@ -116,7 +116,7 @@ get_template_part(
 		'services' => array(
 			array(
 				'title' => null,
-				'items' => array( 'Graphic Design', 'Web Design', 'Web Design', 'Web Development' ),
+				'items' => array( 'Graphic Design', 'Storybranding', 'Web Design', 'Web Development' ),
 			),
 			array(
 				'title' => null,
@@ -127,44 +127,44 @@ get_template_part(
 );
 ?>
 <section class="statistics">
-	<div class="container">
-		<div class="row text-center justify-content-around gap-5">
-			<div class="col-6 col-md-4 col-xxl-2">
-				<?php k1_get_svg_asset( 'marcom-stats-21' ); ?>
-				<p>21% of websites report trouble with low traffic.</p>
-			</div>
-			<div class="col-6 col-md-4 col-xxl-2">
-				<?php k1_get_svg_asset( 'marcom-stats-47' ); ?>
-				<p>In North America, 51.2% of web traffic comes from mobile devices.</p>
-			</div>
-			<div class="col-6 col-md-4 col-xxl-2">
-				<?php k1_get_svg_asset( 'marcom-stats-51' ); ?>
-				<p>47% of users won't wait longer than two seconds for a website to load.</p>
-			</div>
-			<div class="col-6 col-md-4 col-xxl-2">
-				<?php k1_get_svg_asset( 'marcom-stats-88' ); ?>
-				<p>Including videos on a website can increase time spent on the page by 88%.</p>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row text-center justify-content-around gap-5">
+            <div class="col-6 col-md-4 col-xxl-2">
+                <?php k1_get_svg_asset( 'marcom-stats-21' ); ?>
+                <p>21% of websites report trouble with low traffic.</p>
+            </div>
+            <div class="col-6 col-md-4 col-xxl-2">
+                <?php k1_get_svg_asset( 'marcom-stats-47' ); ?>
+                <p>In North America, 51.2% of web traffic comes from mobile devices.</p>
+            </div>
+            <div class="col-6 col-md-4 col-xxl-2">
+                <?php k1_get_svg_asset( 'marcom-stats-51' ); ?>
+                <p>47% of users won't wait longer than two seconds for a website to load.</p>
+            </div>
+            <div class="col-6 col-md-4 col-xxl-2">
+                <?php k1_get_svg_asset( 'marcom-stats-88' ); ?>
+                <p>Including videos on a website can increase time spent on the page by 88%.</p>
+            </div>
+        </div>
+    </div>
 </section>
 <section class="recent-partnerships">
-	<div class="container">
-		<div class="row justify-content-center my-5 py-5">
-			<div class="col-lg-10 text-lg-center">
-				<h2 class='color-grey'>Recent Partnerships</h2>
-				<p>We have had the privilege of partnering with so many amazing ministries. Below is some of the recent work that we have had the opportunity to be a part of.</p>
-			</div>
-		</div>
-		<div class="western my-5">
-			<div class="row justify-content-center my-5">
-				<div class="col-lg-8 text-center">
-					<img class='western-logo w-75' src="<?php k1_get_image_asset_url( 'logo-western', 'png', 'previous-work/western-christian', ); ?>" />
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-6 mb-5 mb-lg-0">
-					<?php
+    <div class="container">
+        <div class="row justify-content-center my-5 py-5">
+            <div class="col-lg-10 text-lg-center">
+                <h2 class='color-grey'>Recent Partnerships</h2>
+                <p>We have had the privilege of partnering with so many amazing ministries. Below is some of the recent work that we have had the opportunity to be a part of.</p>
+            </div>
+        </div>
+        <div class="western my-5">
+            <div class="row justify-content-center my-5">
+                <div class="col-lg-8 text-center">
+                    <img class='western-logo w-75' src="<?php k1_get_image_asset_url( 'logo-western', 'png', 'previous-work/western-christian', ); ?>" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <?php
 					get_template_part(
 						'template-parts/sliders/swiper',
 						'project-gallery',
@@ -179,10 +179,10 @@ get_template_part(
 						)
 					);
 					?>
-					<h3 class="text-center">Before</h3>
-				</div>
-				<div class="col-lg-6">
-					<?php
+                    <h3 class="text-center">Before</h3>
+                </div>
+                <div class="col-lg-6">
+                    <?php
 					get_template_part(
 						'template-parts/sliders/swiper',
 						'project-gallery',
@@ -199,11 +199,11 @@ get_template_part(
 						)
 					);
 					?>
-					<h3 class="text-center">After</h3>
-				</div>
-			</div>
-			<div class="row justify-content-around">
-				<?php
+                    <h3 class="text-center">After</h3>
+                </div>
+            </div>
+            <div class="row justify-content-around">
+                <?php
 				$content->cta_button(
 					array(
 						'text'        => 'Final Site Design',
@@ -213,17 +213,17 @@ get_template_part(
 					)
 				);
 				?>
-			</div>
-		</div>
-		<div class="beachpoint my-5">
-			<div class="row justify-content-center my-5">
-				<div class="col-lg-8 text-center">
-					<img class='western-logo w-75' src="<?php k1_get_image_asset_url( 'beachpoint-logo', 'png', 'previous-work/beachpoint', ); ?>" />
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-6 mb-5 mb-lg-0">
-					<?php
+            </div>
+        </div>
+        <div class="beachpoint my-5">
+            <div class="row justify-content-center my-5">
+                <div class="col-lg-8 text-center">
+                    <img class='western-logo w-75' src="<?php k1_get_image_asset_url( 'beachpoint-logo', 'png', 'previous-work/beachpoint', ); ?>" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <?php
 					get_template_part(
 						'template-parts/sliders/swiper',
 						'project-gallery',
@@ -237,10 +237,10 @@ get_template_part(
 						)
 					);
 					?>
-					<h3 class="text-center">Before</h3>
-				</div>
-				<div class="col-lg-6">
-					<?php
+                    <h3 class="text-center">Before</h3>
+                </div>
+                <div class="col-lg-6">
+                    <?php
 					get_template_part(
 						'template-parts/sliders/swiper',
 						'project-gallery',
@@ -258,13 +258,13 @@ get_template_part(
 						)
 					);
 					?>
-					<h3 class="text-center">After</h3>
-				</div>
-			</div>
+                    <h3 class="text-center">After</h3>
+                </div>
+            </div>
 
-		</div>
-		<div class="row justify-content-around">
-			<?php
+        </div>
+        <div class="row justify-content-around">
+            <?php
 			$content->cta_button(
 				array(
 					'text'        => 'Final Site Design',
@@ -274,8 +274,8 @@ get_template_part(
 				),
 			);
 			?>
-		</div>
-	</div>
+        </div>
+    </div>
 </section>
 <?php get_template_part( 'template-parts/final-cta/content', 'daunting' );
 ?>
